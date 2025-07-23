90ec4132b6b97523a07ac774a2babd4a27c61b88
asafwat95 <A.safwat5595@gmail.com>
2025-07-22 20:56:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 import os
 
 # إعداد المتغيرات من البيئة أو استبدلهم يدويًا بالقيم
-TELEGRAM_BOT_TOKEN = "7974713193:AAGaE-sjvB7kTAt_yg6Mp68_xE5lC_czdA8"
-TELEGRAM_CHAT_ID =  "7287044246"
+TELEGRAM_BOT_TOKEN = "7728837149:AAH5OD6f0NgfgU0zBJKUxmHPYooiRtvGrrM"
+TELEGRAM_CHAT_ID =  "-1002525560355"
 
 # نص الرسالة الترحيبية
 message = "👋 أهلاً بك! هذا اختبار لإرسال رسالة ترحيبية عبر البوت على Telegram."
